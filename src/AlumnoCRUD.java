6a300b47dde42a2e15a08e2fbf560683016b0fff
Ricardo <ruperdink125@gmail.com>
2023-11-22 19:48:19 +0100

    Añadido método altaAlumno

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -56,5 +56,11 @@ public class AlumnoCRUD {
         return listaAlumnos;
     }
 
+public void altaAlumno(Alumno alumno) {
+        ArrayList<Alumno> listaAlumno = leerAlumnos();
+        listaAlumno.add(alumno);
+        escribirAlumnos(listaAlumno);
+}
+
 
 }
