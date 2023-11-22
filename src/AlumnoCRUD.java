a5075c5d01afe3fa31041fb97bff0248df360c34
Manuel Garrido <manuelgarridoserrano120@gmail.com>
2023-11-22 19:51:09 +0100

    Añadido método bajaAlumno

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -22,6 +22,16 @@ public class AlumnoCRUD {
             System.err.println(e.getMessage());
         }
     }
+
+    public void bajaAlumno(String dni) {
+        ArrayList<Alumno> listaAlumno = leerAlumnos();
+        for (int i = 0; i < listaAlumno.size(); i++) {
+            if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni)) {
+                listaAlumno.remove(i);
+            }
+        }
+        escribirAlumnos(listaAlumno);
+    }
     
     /*
         Escribe en el fichero un ArrayList de alumnos
